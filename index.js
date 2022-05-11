84cc2f2e3dc546d1ac06b0d1e70856a20a5185a6
Oktay ahras <102553352+oktayahras@users.noreply.github.com>
2022-05-11 17:31:56 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tanıtım</title>
+</head>
+<body>
+    <h2>TANITIM</h2>
+    <p>Ben Oktay Ahras.Mersin Üniversitesinde Bilişim Sistemleri ve Teklonojileri bölümü okuyorum.Kendimi bilgisayar yazılımı ve donanımı konusunda geliştirmek istiyorum. </p>   
+    <h2>ŞU AN NE YAPIYORUM</h2>
+    <P>Şu an da wep tasarım alanında,HTML,CSS,JS ile ilgileniyorum.Bunun dışında C++,c# ile ilgileniyorum. </P>
+
+
+    <div class="row">
+        <div class="col-6">
+          <h2>KONUM</h2>
+          <P>Bootstrap ile listeler ve kartlar.</P>
+          <a
+          href="./ödev.html" 
+          class="btn btn-primary"
+          >PROJECT</a>
+        </div>
+
+
+ </body>
+</html>
